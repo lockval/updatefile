4c17fe8c9fe3447c2f270b6cae52939ba56d088f
vanishs <vanishs@hotmail.com>
2022-10-20 14:59:23 +0800

    main platform

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 
-GOOS=windows    GOARCH=amd64 go build -trimpath -o updatefile.exe   .
-GOOS=linux      GOARCH=amd64 go build -trimpath -o updatefile       .
+GOOS=windows    GOARCH=arm64 go build -trimpath -o updatefile.arm64.exe .
+GOOS=windows    GOARCH=amd64 go build -trimpath -o updatefile.amd64.exe .
+GOOS=linux      GOARCH=arm64 go build -trimpath -o updatefile.arm64.lin .
+GOOS=linux      GOARCH=amd64 go build -trimpath -o updatefile.amd64.lin .
+GOOS=darwin     GOARCH=arm64 go build -trimpath -o updatefile.arm64.mac .
+GOOS=darwin     GOARCH=amd64 go build -trimpath -o updatefile.amd64.mac .
