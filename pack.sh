4e805f7ac8427a73339bb7fd3bd0b39e0a9713bb
vanishs <vanishs@hotmail.com>
2022-11-10 21:31:10 +0800

    fix golang version

--- pack.sh
+++ pack.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-go build .
-GOOS=linux GOARCH=amd64 go build .
+GOOS=linux GOARCH=amd64 go build -o updatefile.amd64.lin .
+GOOS=linux GOARCH=arm64 go build -o updatefile.arm64.lin .
+GOOS=windows GOARCH=amd64 go build -o updatefile.amd64.exe .
+GOOS=windows GOARCH=arm64 go build -o updatefile.arm64.exe .
+GOOS=darwin GOARCH=amd64 go build -o updatefile.amd64.mac .
+GOOS=darwin GOARCH=arm64 go build -o updatefile.arm64.mac .
