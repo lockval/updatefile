b9681079af1133022b28a2b9853cb5c962d98209
vanishs <vanishs@hotmail.com>
2022-11-12 09:49:14 +0800

    update trimpath

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ GOOS=windows    GOARCH=arm64 go build -trimpath -o updatefile.arm64.exe .
 GOOS=windows    GOARCH=amd64 go build -trimpath -o updatefile.amd64.exe .
 GOOS=linux      GOARCH=arm64 go build -trimpath -o updatefile.arm64.lin .
 GOOS=linux      GOARCH=amd64 go build -trimpath -o updatefile.amd64.lin .
-GOOS=darwin     GOARCH=arm64 go build -trimpath -o updatefile.arm64.mac .
-GOOS=darwin     GOARCH=amd64 go build -trimpath -o updatefile.amd64.mac .
+GOOS=darwin     GOARCH=arm64 go build -trimpath -o updatefile.arm64.app .
+GOOS=darwin     GOARCH=amd64 go build -trimpath -o updatefile.amd64.app .
--- pack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-GOOS=linux GOARCH=amd64 go build -o updatefile.amd64.lin .
-GOOS=linux GOARCH=arm64 go build -o updatefile.arm64.lin .
-GOOS=windows GOARCH=amd64 go build -o updatefile.amd64.exe .
-GOOS=windows GOARCH=arm64 go build -o updatefile.arm64.exe .
-GOOS=darwin GOARCH=amd64 go build -o updatefile.amd64.mac .
-GOOS=darwin GOARCH=arm64 go build -o updatefile.arm64.mac .
